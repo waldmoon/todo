bd63d98f7fa1c56fda41e12245fcb334f4233a9c
user <muhamadalfariji27@gmail.com>
2026-07-12 18:23:20 +0700

    Configure Supabase credentials

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Isi dengan kredensial dari dashboard Supabase (Project Settings → API)
 window.TODO_CONFIG = {
-  SUPABASE_URL: "ISI_URL_SUPABASE",
-  SUPABASE_ANON_KEY: "ISI_ANON_KEY",
+  SUPABASE_URL: "https://vlkxjsystzkhhtzvzylr.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsa3hqc3lzdHpraGh0enZ6eWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTA0OTIsImV4cCI6MjA5OTQyNjQ5Mn0.9zFJQarFB1VAI8J5flmaVX8WNdAyv3-PkCaBxYi-SQs",
 };
